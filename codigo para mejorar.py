09c9f486c593eb902cc1d2471c871be48f879908
hugobonforte <hugobonforte@gmail.com>
2026-06-09 14:39:08 -0600

    Add files via upload

---
+++ codigo para mejorar.py
@@ -0,0 +1,156 @@
+# Si alguien esta viendo esto hice este proyecto gracias al aburrimiento
+elige = 0
+import time
+import random
+
+time.sleep(elige)
+
+monedas = 0
+reputacion = 5
+texto_amigo = "Robarle el dinero a tu amigo"
+texto_opcion = "Di :robar: para robar a su amigo o di :vender: para vender frutas en su emprendimiento "
+
+print(
+    "Reglas: Escribir justo como lo dice el bot osea si no tiene mayusculas escribelo sin mayus, Tambien en este juego pasan cosas segun tu suerte osea: Random ")
+time.sleep(6)
+Nombre = input("¿Cual es tu nombre? ")
+
+while True:
+    print(
+        "hola",
+        Nombre,
+        "este programa es simple voy a actualizar este juego ademas de eso vas a tener opciones, este es tu Dinero:",
+        monedas,
+        "esta es tu reputacion",
+        reputacion,
+        "\n Estas vendiendo frutas",
+    )
+
+    time.sleep(6)
+
+    print(
+        "puedes \n",
+        texto_amigo,
+        "\n Vender frutas en tu nuevo emprendimiento donde vendes frutas en el cual gastaste 100 monedas",
+    )
+    elige = input(texto_opcion)
+
+    print("Cargando.")
+    time.sleep(1)
+    print("Cargando..")
+    time.sleep(1)
+    print("Cargando...")
+    time.sleep(1)
+    print("Cargando.")
+    time.sleep(1)
+    print("Cargando..")
+    time.sleep(1)
+    print("Cargando...")
+
+    time.sleep(1)
+
+    elige_random = random.randint(1, 5)
+    clientes = random.randint(1, 8)
+    clientes1 = random.randint(20, 30)
+    clientes2 = random.randint(10, 20)
+    monedas1 = random.randint(30, 50)
+    monedas2 = random.randint(10, 25)
+    monedas3 = random.randint(5, 10)
+    reputacion1 = random.randint(1, 5)
+
+    if elige == "vender" and clientes >= 1 and clientes <= 3:
+        monedas += monedas1
+        reputacion += 2
+        print(
+            "Tu negocio le fue muy bien tuviste ",
+            clientes1,
+            " clientes este dia y solo es el primero ganaste",
+            monedas1,
+            "monedas, Monedas:",
+            monedas,
+            "ademas tu reputacion subio un",
+            reputacion1,
+            ",de reputacion:",
+            reputacion,
+        )
+
+    elif elige == "vender" and clientes >= 4 and clientes <= 6:
+        monedas += monedas2
+        reputacion += reputacion1
+        print(
+            "Tu negocio le fue normal tuviste ",
+            clientes2,
+            "clientes este dia y solo es el primero igual no es tanto es poco, ganaste",
+            monedas2,
+            "monedas, tus Monedas:",
+            monedas,
+            "ademas tu reputacion subio un",
+            reputacion1,
+            "de reputacion:",
+            reputacion,
+        )
+
+    elif elige == "vender" and clientes >= 7 and clientes <= 8:
+        monedas += monedas2
+        reputacion += reputacion1
+        print(
+            "Tu negocio le fue muy mal obtuviste ",
+            clientes2,
+            "no es tanto es muy poco, ganaste",
+            monedas3,
+            "monedas, tus Monedas:",
+            monedas,
+            "ademas tu reputacion subio un",
+            reputacion1,
+            "de reputacion:",
+            reputacion,
+        )
+
+    if (
+        elige == "robar"
+        and elige_random == 1
+        or elige == "robar"
+        and elige_random == 2
+        or elige == "robar"
+        and elige_random == 3
+    ):
+        print(
+            "Te encontraron quedas arrestado Estas arrestado por 30 segundos Si esperas podras seguir el juego"
+        )
+        time.sleep(15)
+        print("Cargando.")
+        time.sleep(1)
+        print("Cargando..")
+        time.sleep(1)
+        print("Cargando...")
+        time.sleep(1)
+        print("Cargando.")
+        time.sleep(1)
+        print("Cargando..")
+        time.sleep(1)
+        print("Cargando...")
+        monedas -= 50
+        reputacion -= 5
+        print(
+            "Has salido de tu condena pero como castigo tienes una deuda de 50 dolares Ademas tu amigo te abandono y ademas has perdido 5 de reputacion",
+            reputacion,
+            " Ahora tus monedas son",
+            monedas,
+        )
+        texto_amigo = "Robarle el dinero a un desconocido en la calle"
+        texto_opcion = "Diga :robar: para robar a un desconocido o diga :vender: para vender frutas en su emprendimiento "
+
+    elif (
+        elige == "robar" and elige_random == 5 or elige == "robar" and elige_random == 4
+    ):
+        monedas += 50
+        print(
+            "Uffff has tenido suerte no te han pillado!!! Con eso me referia a suerte, ahora tus monedas son",
+            monedas,
+        )
+
+    if monedas >= 500:
+        break
+
+    if monedas <= -150:
+        break
